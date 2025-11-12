dbf4b49d3fc84b058ed02b110b5c0a37fb42b13c
shayak-farooq <SHAYAKFAROOQ52@GMAIL.COM>
2025-11-12 11:14:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const userRoutes = require('./routes/userRoutes')
+const mongoose = require('mongoose');
+require('dotenv/config')
+const app = express()
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+// connect mongodb
+if(!MONGO_URI){
+    console.error("MONGO_URI missing from .env")
+    process.exit(1)
+}
+mongoose.connect(MONGO_URI)
+.then(()=> console.log("Mongodb connected successfully"))
+.catch(err => {
+    console.error("Mongodb connection error:",err)
+    process.exit(1)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/api',userRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
